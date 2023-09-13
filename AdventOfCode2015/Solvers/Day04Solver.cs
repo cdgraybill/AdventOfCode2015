b99604c884b40564ae68f4426d45873ab03a9f96
cdgraybill <cdgraybill@gmail.com>
2023-09-12 21:29:16 -0500

    day 4 complete

---
+++ AdventOfCode2015/Solvers/Day04Solver.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2015.Solvers
+{
+    public class Day04Solver
+    {
+        private readonly string _problemInput = File.ReadAllText(@"C:\Dev Projects\AdventOfCode2015\AdventOfCode2015\ProblemInputs\Day04Input.txt");
+        private int number = 0;
+
+        public int Solve_Part01()
+        {
+            var isStillLooking = true;
+
+            while (isStillLooking)
+            {
+                var computeInput = $"{_problemInput}{number}";
+                byte[] encodedPassword = new UTF8Encoding().GetBytes(computeInput);
+                byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+                string encoded = BitConverter.ToString(hash)
+                    .Replace("-", string.Empty)
+                    .ToLower();
+                var firstFive = encoded[..5];
+
+                if (firstFive == "00000")
+                    break;
+                else
+                    number++;
+            }
+
+            return number;
+        }
+
+        public int Solve_Part02()
+        {
+            var isStillLooking = true;
+
+            while (isStillLooking)
+            {
+                var computeInput = $"{_problemInput}{number}";
+                byte[] encodedPassword = new UTF8Encoding().GetBytes(computeInput);
+                byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+                string encoded = BitConverter.ToString(hash)
+                    .Replace("-", string.Empty)
+                    .ToLower();
+                var firstSix = encoded[..6];
+
+                if (firstSix == "000000")
+                    break;
+                else
+                    number++;
+            }
+
+            return number;
+        }
+    }
+}
---
+++ AdventOfCode2015Tests/SolverTests/Day04Tests.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode2015.Solvers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2015Tests.SolverTests
+{
+    public class Day04Tests
+    {
+        [Fact]
+        public void Solve_Part01()
+        {
+            var solver = new Day04Solver();
+            var answer = solver.Solve_Part01();
+
+            Assert.Equal(2, answer);
+        }
+
+        [Fact]
+        public void Solve_Part02()
+        {
+            var solver = new Day04Solver();
+            var answer = solver.Solve_Part02();
+
+            Assert.Equal(2572, answer);
+        }
+    }
+}
