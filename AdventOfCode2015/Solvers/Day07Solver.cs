547668272d081bf420017b80160560d67bc08c37
cdgraybill <cdgraybill@gmail.com>
2023-10-16 21:11:41 -0500

    day 7 files

---
+++ AdventOfCode2015/Solvers/Day07Solver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2015.Solvers
+{
+    public class Day07Solver
+    {
+        private readonly List<string> _problemInput = File.ReadLines(@"C:\Dev Projects\AdventOfCode2015\AdventOfCode2015\ProblemInputs\Day07Input.txt").ToList();
+        private int _numberOfLights = 0;
+    }
+}
